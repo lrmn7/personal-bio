8687a555626fe7a6909971036416b2a4d59b8d03
lrmn7 <lrmnid@gmail.com>
2023-07-06 19:55:48 +0700

    main

--- components/Misc/NowPlaying.misc.tsx
+++ components/Misc/NowPlaying.misc.tsx
@@ -7,12 +7,16 @@ import Link from "next/link";
 import useSWR from "swr";
 import { fetcher } from "../../lib/fetcher";
 
-const SpotifyCard: NextComponentType = () => {
+interface SpotifyCardProps {
+  data: NowPlayingSong;
+}
+
+const SpotifyCard: NextComponentType<{}, {}, SpotifyCardProps> = () => {
   const { data, error } = useSWR<NowPlayingSong>("/api/now-playing", fetcher);
 
-  function truncateTitle(title) {
+  function truncateTitle(title?: string) {
     if (!title) {
-      return;
+      return "";
     }
 
     const words = title.split(" ");
@@ -21,37 +25,34 @@ const SpotifyCard: NextComponentType = () => {
       return title;
     }
 
-    return `${words[0]} ${words[1]} ${words[2]} ${words[3]} ...`;
+    return `${words.slice(0, 4).join(" ")} ...`;
   }
 
   const truncatedTitle = truncateTitle(data?.title);
   const truncatedArtist = truncateTitle(data?.artist);
 
   return (
-    <>
-      <div className="font-sen mb-8 flex flex-col items-center gap-2 text-center text-sm text-gray-300">
-        <p className="text-white">See what I'm currently listening on spotify</p>
-        <div className="flex flex-row items-center gap-x-2">
-          <Image
-            src="/assests/spotify.svg"
-            width="20"
-            height="20"
-            alt="spotify icon"
-          />
-          {data?.isPlaying ? (
-            <Link href={data?.songUrl} passHref>
-              <p className="cursor-pointer truncate">
-                <span className="text-white">{truncatedTitle}</span>
-              </p>
-            </Link>
-          ) : (
-            <p>No listening to anything</p>
-          )}
-        </div>
+    <div className="font-sen mb-8 flex flex-col items-center gap-2 text-center text-sm text-gray-300">
+      <p className="text-white">See what I&apos;m currently listening on Spotify</p>
+      <div className="flex flex-row items-center gap-x-2">
+        <Image
+          src="/assets/spotify.svg"
+          width={20}
+          height={20}
+          alt="spotify icon"
+        />
+        {data?.isPlaying ? (
+          <Link href={data?.songUrl} passHref>
+            <p className="cursor-pointer truncate">
+              <span className="text-white">{truncatedTitle}</span>
+            </p>
+          </Link>
+        ) : (
+          <p>No listening to anything</p>
+        )}
       </div>
-    </>
+    </div>
   );
-  
 };
 
 export default SpotifyCard;
