c0b2a4eb9ea2c54a5b3bc07d599f67484b7fb51c
lrmn7 <lrmn.dev@gmail.com>
2023-10-12 06:01:11 +0700

    🔥

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, "styles")],
-  // },
+  sassOptions: {
+  includePaths: [path.join(__dirname, "styles")],
+  },
   images: {
     domains: ["cdn.discordapp.com"],
   },
