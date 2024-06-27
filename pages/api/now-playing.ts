d20313588589f5018d9a2af9f417c9efefcc0648
lrmn7 <lrmn.dev@gmail.com>
2024-06-27 13:25:25 +0700

    Updated now-playing API handler to use Edge runtime and return Response objects instead of using NextApiResponse

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -1,41 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { getNowPlaying } from "../../lib/spotify";
+
 export const runtime = 'edge';
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const response = await getNowPlaying();
 
-  if (response.status === 204 || response.status > 400) {
-    return res.status(200).json({ isPlaying: false });
-  }
+export default async function handler(req: Request) {
+    const response = await getNowPlaying();
 
-  const song = await response.json();
-  if (song.item === null) {
-    return res.status(200).json({ isPlaying: false });
-  }
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists
-    .map((_artist: any) => _artist.name)
-    .join(", ");
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+    if (response.status === 204 || response.status > 400) {
+      return new Response(JSON.stringify({ isPlaying: false }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
 
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=60, stale-while-revalidate=30"
-  );
+    const song = await response.json();
+    if (song.item === null) {
+      return new Response(JSON.stringify({ isPlaying: false }), {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
 
-  return res.status(200).json({
-    album,
-    albumImageUrl,
-    artist,
-    isPlaying,
-    songUrl,
-    title,
-  });
+    const isPlaying = song.is_playing;
+    const title = song.item.name;
+    const artist = song.item.artists.map((artist: any) => artist.name).join(", ");
+    const album = song.item.album.name;
+    const albumImageUrl = song.item.album.images[0].url;
+    const songUrl = song.item.external_urls.spotify;
+
+    const headers = new Headers({
+      "Cache-Control": "public, s-maxage=60, stale-while-revalidate=30",
+      "Content-Type": "application/json"
+    });
+
+    return new Response(JSON.stringify({
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+    }), { headers });
 }
 
